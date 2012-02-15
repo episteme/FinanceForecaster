d22f60e220b7d31b426f8d0e825a22297db63303
kornel <kornel@dcs>
2012-02-15 13:10:39 +0000

    added third

--- Team2/src/TestClass.java
+++ Team2/src/TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Stocks are trending!");
 		System.out.println("Goodbye!");
+		System.out.println("Third");
 	}
 }
