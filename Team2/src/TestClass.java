2dc0bff16006f5d76f0f6581779f43146fdd99ec
Marcus <marcus@cs>
2012-02-15 13:07:11 +0000

    Added Second Line

--- Team2/src/TestClass.java
+++ Team2/src/TestClass.java
@@ -1,6 +1,7 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Stocks are trending!");
+		System.out.println("Goodbye!");
 	}
 }
