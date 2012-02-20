e729eb3e071feec5a13f54c5f3612ffd5696773a
Marcus <marcus@cs>
2012-02-20 18:27:55 +0000

    Little cleanup

--- Team2/src/MainNode.java
+++ Team2/src/MainNode.java
@@ -1,15 +1,10 @@
 
 public class MainNode implements Runnable {
 	
-	static Integer i = 1;
-	
 	public static void main(String[] args) {
 		Thread t = new Thread(new MainNode());
 		t.start();
-		System.out.println("Hello tes123");
-		i = 2;
-		Thread p = new Thread(new MainNode());
-		p.start();
+		System.out.println("End of main");
 	}
 	
 	MainNode() {
@@ -17,13 +12,8 @@ public class MainNode implements Runnable {
 	}
 	
 	public void run() {
-		if (i == 1) {
-			System.out.println("Hello tes123 run");
-			MultipleSocketServer obj = new MultipleSocketServer();
-			obj.go();
-		}
-		if (i == 2) {
-			System.out.println("Poo in my shoe");
-		}
+		System.out.println("Start of run");
+		MultipleSocketServer obj = new MultipleSocketServer();
+		obj.go();
 	}
 }
