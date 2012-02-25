c1ef1391c06c1407c059f907fada9c321f99daf1
marcus <marcus@air>
2012-02-25 20:48:15 +0000

    Created initial Topic and Article classes

---
+++ Team2/src/Article.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+
+// An Article contains a link to an article
+// and the date it was picked up
+public class Article {
+	
+	private String URL;
+	private Date date;
+	
+	public Article(String URL, Date date) {
+		this.URL = URL;
+		this.date = date;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	
+
+}
---
+++ Team2/src/Topic.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+// A Topic is the large concept of a story,
+// it is a collection of relevant keywords,
+// links to the relevant articles and the
+// dates these articles were picked up
+public class Topic {
+	
+	private HashMap<String, Double> words;
+	private LinkedList<Article> articles;
+	
+	// Constructor, begins list with first article
+	public Topic(HashMap<String, Double> words, Article article) {
+		this.words = words;
+		this.articles = new LinkedList<Article>();
+		articles.push(article);
+	}
+	
+	public void addArticle(Article article) {
+		articles.push(article);
+	}
+	
+	public void addWord(String s, Double d) {
+		words.put(s, d); 
+	}
+
+	public HashMap<String, Double> getWords() {
+		return words;
+	}
+
+	public LinkedList<Article> getArticles() {
+		return articles;
+	}
+	
+	
+}
