e083ecde9e15d46a4344accdad68923eddd54b4c
Marcus <marcus@cs>
2012-03-05 16:09:29 +0000

    Initial commit of display code

--- MainApp/src/team2/mainapp/MainAppActivity.java
+++ MainApp/src/team2/mainapp/MainAppActivity.java
@@ -1,30 +1,55 @@
 package team2.mainapp;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainAppActivity extends Activity {
-
+	private Handler handler;
+	private ProgressBar progress;
 	static String s;
+	
+	private ListView lv;
 
-	/** Called when the activity is first created. */
+	ArrayAdapter<String> ads;
+	
+/** Called when the activity is first created. */
+
 	@Override
-	public void onCreate(Bundle icicle) {
-
-
-		super.onCreate(icicle);
-
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Log.d("Debug", "onCreate");		
+		progress = (ProgressBar) findViewById(R.id.progressBar1);
+		handler = new Handler();
+		
+		final ArrayList<String> al = new ArrayList<String>();
+		
+		al.add("penile");
+		al.add("vagenda");
+		
+		lv = (ListView)	findViewById(R.id.listView1);
+		ads = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+		lv.setAdapter(ads);
+		
+	}
 
+	public void startProgress(View view) {
+		// Do something long
 		Runnable runnable = new Runnable() {
+			@Override
 			public void run() {
-				String date = "1212/12/12 12:12:12";
-				while (true) {
+				for (int i = 0; i <= 10; i++) {
+					final int value = i;
+					String date = "1212/12/12 12:12:12";
 					try {
 						Log.d("Debug", "Thread1");
 						Thread.sleep(10000);
@@ -32,32 +57,20 @@ public class MainAppActivity extends Activity {
 						s = TCPClient.go(date);
 						DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/d HH:mm:ss");
 						date = dateFormat.format(dateType);
-						Log.d("Debug", "Finished?");
+						Log.d("Debug", s);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
+					handler.post(new Runnable() {
+						@Override
+						public void run() {
+							progress.setProgress(value);
+							ads.add(s);
+						}
+					});
 				}
 			}
 		};
 		new Thread(runnable).start();
-
-		Runnable runnable2 = new Runnable() {
-			public void run(){
-				while (true) {
-					try {
-						Log.d("Debug", "Thread2");
-						Thread.sleep(10000);
-					}
-					catch (Exception e) {}
-					if (s == null)
-						continue;
-					Log.d("Debug", s);
-					s = null;
-				}
-			}
-		};
-		new Thread(runnable2).start();
-
-	Log.d("On Create", "onCreate finished");
 	}
 }
