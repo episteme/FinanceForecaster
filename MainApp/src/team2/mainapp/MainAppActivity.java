6970d53338701e4279916829fe1d30aaf37d2791
Toby <Toby@10.131.156.24>
2012-03-06 20:00:49 +0000

    Android now parses the input correctly, and outputs individual items

--- MainApp/src/team2/mainapp/MainAppActivity.java
+++ MainApp/src/team2/mainapp/MainAppActivity.java
@@ -16,14 +16,15 @@ import android.widget.ProgressBar;
 
 public class MainAppActivity extends Activity {
 	private Handler handler;
+	static LinkedList<LinkedList<Topic>> allTopics; 
 	private ProgressBar progress;
 	static String s;
-	
+
 	private ListView lv;
 
 	ArrayAdapter<String> ads;
-	
-/** Called when the activity is first created. */
+
+	/** Called when the activity is first created. */
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,16 +32,16 @@ public class MainAppActivity extends Activity {
 		setContentView(R.layout.main);
 		progress = (ProgressBar) findViewById(R.id.progressBar1);
 		handler = new Handler();
-		
+
 		final ArrayList<String> al = new ArrayList<String>();
-		
+
 		al.add("penile");
 		al.add("vagenda");
-		
+
 		lv = (ListView)	findViewById(R.id.listView1);
 		ads = new ArrayAdapter<String>(this, R.layout.rowlayout, R.id.labelo, al);
 		lv.setAdapter(ads);
-		
+
 	}
 
 	public void startProgress(View view) {
@@ -53,16 +54,15 @@ public class MainAppActivity extends Activity {
 					String date = "1212/12/12 12:12:12";
 					try {
 						Log.d("Debug", "Thread1");
-						Thread.sleep(10000);
-						Date dateType = new Date();
 						s = TCPClient.go(date);
-						
+						Date dateType = new Date();
+						Log.d("Debug", s);
 						parseInput(s);
-						
-					
 						DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/d HH:mm:ss");
 						date = dateFormat.format(dateType);
 						Log.d("Debug", s);
+						Thread.sleep(30000);
+
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -70,7 +70,13 @@ public class MainAppActivity extends Activity {
 						@Override
 						public void run() {
 							progress.setProgress(value);
-							ads.add(s);
+							ads.clear();
+							for(LinkedList<Topic> topicsector : allTopics){
+								for(Topic topic : topicsector){
+									String allInfo = topic.getTitle() + topic.getDate();
+									ads.add(allInfo);
+								}
+							}
 						}
 					});
 				}
@@ -78,25 +84,29 @@ public class MainAppActivity extends Activity {
 		};
 		new Thread(runnable).start();
 	}
-	
-	LinkedList<LinkedList<Topic>> allTopics = new LinkedList<LinkedList<Topic>>(); 
+
 
 	protected void parseInput(String s2) {
+		allTopics = new LinkedList<LinkedList<Topic>>(); 
+
+		Log.d("String", s2);
 		String[] type = s2.split("SPLITINFO\n");
 		String[] topicsectors = type[1].split("TOPSTOP\n");
-		
+		Log.d("Type1", type[0]);
+		Log.d("Type2", type[1]);
 		for (String sector : topicsectors)
 		{
+			Log.d("Sector", sector);
 			LinkedList<Topic> topicSector = new LinkedList<Topic>();
-			
+
 			String[] topics = sector.split("SPECTOPS\n");
 			for (String topic : topics)
 			{
-				
+				Log.d("Topic", topic);
 				String[] rawData = topic.split(";;\n");
 				String[] links = rawData[2].split(";\n");
 				String[] words = rawData[3].split(";\n");
-				
+
 				ArrayList<String> URLS = new ArrayList<String>();
 				for (String link : links)
 				{
@@ -104,7 +114,7 @@ public class MainAppActivity extends Activity {
 					for(String bit : bits)
 						URLS.add(bit);
 				}
-				
+
 				ArrayList<KeyWord> KeyWords = new ArrayList<KeyWord>();
 				for (String word : words)
 				{
@@ -112,12 +122,12 @@ public class MainAppActivity extends Activity {
 					for (int i = 0; i < bits.length; i += 2)
 						KeyWords.add(new KeyWord(bits[i], bits[i+1]));
 				}
-				
+
 				topicSector.add(new Topic(rawData[0],rawData[1],URLS,KeyWords));
 			}
-			
+
 			allTopics.add(topicSector);
 		}
-		
+
 	}
 }
