4cd6e3cc62d3be719c87ccd3ff5102893a89ba55
Toby <Toby@10.131.156.61>
2012-04-24 14:31:19 +0100

    Lol

---
+++ MainApp/src/team2/mainapp/SingleTopic.java
@@ -0,0 +1,12 @@
+package team2.mainapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SingleTopic extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
