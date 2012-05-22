5d5615a239ba9ec1581b82a9ea1c4103a5cc9b43
Marcus <marcus@cs>
2012-05-22 16:22:30 +0100

    jsoup fix

--- Team2/src/Topic.java
+++ Team2/src/Topic.java
@@ -172,9 +172,9 @@ public class Topic implements Comparable<Topic> {
 	    String rString = "";
 	    for (int i = 1; i <= j; i++) {
 	    	WordAndVal nextWord = wavl.get(wavl.size() - i);
-	    	rString = rString + nextWord.getWord() + "@" + words.get(nextWord.getWord()).getSent() + ";\n";
+	    	rString = rString + Jsoup.parse(nextWord.getWord()).text() + "@" + words.get(nextWord.getWord()).getSent() + ";\n";
 	    }
-	    return Jsoup.parse(rString).text();
+	    return rString;
 	}
 	
 	public void printTopLinks() {
